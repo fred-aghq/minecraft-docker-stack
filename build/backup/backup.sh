32dbbfce59a7a852f3d38210aba25080e840958b
Fred Spooner <fred@aghq.uk>
2020-04-16 15:40:41 +0100

    #2 hopefully overviewer is backed up as well

--- build/backup/backup.sh
+++ build/backup/backup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
-echo "hello"
-ls -al /source/minecraft-server
-rm -rf /target/minecraft-server/*
+rm -rf /target/minecraft-server
+rm -rf /target/overviewer-web
+mkdir -p /target/minecraft-server && mkdir /target/overviewer-web
 rsync -azvh --progress /source/minecraft-server/. /target/minecraft-server/.
+rsync -azvh --progress /source/overviewer-web/. /target/overviewer-web/.
+exit 0
